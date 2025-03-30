9bddf0c116d8e7cfe0d183eee11aaefa59526f9c
Robert Juett <robert.juett@student.tafesa.edu.au>
2025-03-30 22:20:18 +1030

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -52,9 +52,12 @@ namespace ProductApps
                 // Display the total charge after the wrap charge
                 totalChargeWrapBlock.Text = totalChargeWithWrap.ToString("0.00");
 
+                // Calculate GST at 10%
+                decimal gst = 0.10m * totalChargeWithWrap;
 
-
-
+                // Display the total charge after GST
+                decimal totalWithGST = totalChargeWithWrap + gst;
+                totalChargeGSTBlock.Text = totalWithGST.ToString("0.00");
             }
             catch (Exception ex)
             {
@@ -72,7 +75,7 @@ namespace ProductApps
             totalPaymentTextBlock.Text = string.Empty;
             totalChargeTextBlock.Text = string.Empty;
             totalChargeWrapBlock.Text = string.Empty;
-
+            totalChargeGSTBlock.Text = string.Empty;
         }
 
         // Close button click event handler
